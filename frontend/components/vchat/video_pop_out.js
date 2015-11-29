7ca89425351181d7468c5434e887781f40c17b8f
sshin <sangshin814@gmail.com>
2015-11-29 10:16:57 -0800

    video search on pop out

--- frontend/components/vchat/video_pop_out.js
+++ frontend/components/vchat/video_pop_out.js
@@ -1,6 +1,6 @@
 var VideoPopOut = React.createClass({
    _videoOnly: function() {
-    window.open(this._getPopOutURL(), '', 'width=831,height=635');
+    window.open(this._getPopOutURL(), '', 'width=831,height=785');
   },
 
   _getPopOutURL: function() {
@@ -10,7 +10,7 @@ var VideoPopOut = React.createClass({
   render: function() {
     return (
       <div id="video-pop-out-wrapper">
-        <Button id="video-only" text="Video Pop Out (video & system message only)"
+        <Button id="video-only" text="Video Pop Out (no video control & no chat)"
                 color="purple" onClick={this._videoOnly} />
       </div>
     );
